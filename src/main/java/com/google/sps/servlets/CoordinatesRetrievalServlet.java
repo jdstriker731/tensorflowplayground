74e56bfec7b12ba06de8d737c3e566231b6ff846
John Dallard <johndallard@google.com>
2020-07-31 14:21:29 -0400

    Make Cam's requested Changes

--- src/main/java/com/google/sps/servlets/CoordinatesRetrievalServlet.java
+++ src/main/java/com/google/sps/servlets/CoordinatesRetrievalServlet.java
@@ -37,7 +37,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/coordinates")
+@WebServlet("/coordinates-retrieval")
 public class CoordinatesRetrievalServlet extends HttpServlet {
    
   // The ID of your GCS bucket
--- src/main/java/com/google/sps/servlets/SpritesheetRetrievalServlet.java
+++ src/main/java/com/google/sps/servlets/SpritesheetRetrievalServlet.java
@@ -37,7 +37,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/spritesheet")
+@WebServlet("/spritesheet-retrieval")
 public class SpritesheetRetrievalServlet extends HttpServlet {
 
   // The ID of your GCS bucket
