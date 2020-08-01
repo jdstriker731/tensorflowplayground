598e0bf0d5476e70c27f65ad9e972395c3dd7db0
Alvin Hermans <halvin@google.com>
2020-08-01 03:12:19 +0000

    load json

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8629d584ffaa06187ba900549997424e.js"
+  "/precache-manifest.14a57a21c6e360469eafb317e260fce4.js"
 );
 
 self.addEventListener('message', (event) => {
