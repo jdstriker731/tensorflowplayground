0b6caf1c253b6b7d1aa8a4a602ffa97afb174217
John Dallard <johndallard@google.com>
2020-08-07 18:29:19 -0400

    Update pom.xml

--- src/main/java/com/google/sps/servlets/SpritesheetRetrievalServlet.java
+++ src/main/java/com/google/sps/servlets/SpritesheetRetrievalServlet.java
@@ -28,8 +28,6 @@ import com.google.appengine.api.users.UserServiceFactory;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
 import com.google.gson.Gson;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
